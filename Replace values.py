807ae1fea7885b2752e8c9cfcdfaca05b5094a4f
ihsancu <65836346+ihsancu@users.noreply.github.com>
2021-05-30 18:56:59 +0300

    Update Replace values.py

--- Replace values.py
+++ Replace values.py
@@ -1,12 +1,6 @@
 num1 = int(input("Enter the first number: ")) 
 num2 = int(input("Enter the second number: "))
-
+num1, num2 = num2, num1
 #Make the first number's value equal to total values
-num1 = num1 + num2
 
-#num2 has num1 value
-num2 = num1 - num2
-
-#num1 has num2 value
-num1 = num1 - num2
-print("replaced value: num1= ",num1, 'num2=',num2)
+print("replaced values: num1= ",num1, 'num2=',num2)
